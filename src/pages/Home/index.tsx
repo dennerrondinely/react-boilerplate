79c691e7d58f77b2a330e2ce902b7a1acd7a43db
Denner Rondinely da Silva <DENNER.SILVA-ext@ab-Inbev.com>
2022-12-12 08:18:03 -0300

    Add public folder

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,5 +1,16 @@
+import Button from 'components/Button';
+import { useHistory } from 'react-router-dom';
+
 const Home = () => {
-  return <div>Hello 👋, I am a Home component.</div>;
+  const { push } = useHistory();
+  return (
+    <div>
+      <div>Hello 👋, I am a Home component.</div> <br />
+      <Button color="white" bg="gray-dark" onClick={() => push('/about')}>
+        Go to About
+      </Button>
+    </div>
+  );
 };
 
 export default Home;
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,12 +1,14 @@
 import { Switch } from 'react-router-dom';
 
 import Home from 'pages/Home';
+import About from 'pages/About';
 
 import Route from './Route';
 
 const Routes = () => (
   <Switch>
-    <Route path="/" component={Home} exact isPrivate />
+    <Route path="/" component={Home} exact />
+    <Route path="/about" component={About} exact />
   </Switch>
 );
 
